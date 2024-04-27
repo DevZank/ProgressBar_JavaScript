e7037dbce2a4d94f736762c9e6a4d8ffb47551c3
Isaac Martins <111926496+DevZank@users.noreply.github.com>
2024-04-27 02:15:10 -0300

    Subindo o Projeto

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+const button = document.querySelector(".button"),
+  text = document.querySelector(".text");
+
+button.addEventListener("click", () => {
+  button.classList.add("progress");
+  text.innerText = "Uploading...";
+
+  setTimeout(() => {
+    button.classList.remove("progress");
+    text.innerText = "Uploaded";
+  }, 8000);
+});
